94b43a6501362ae3f721d7d36d3d1c097e500670
Hendrik Kleinwaechter <hendrik.kleinwaechter@gmail.com>
2016-03-21 19:37:37 +0100

    add example to readme on how to use

--- README.md
+++ README.md
@@ -26,3 +26,46 @@ Initialize the the gem by setting your username and password:
   Booking.username = 'your-username'
   Booking.password = 'your-password'
 ```
+
+Right now only one endpoint is supported.
+
+```
+params = {
+  checkin: Time.now.strftime("%F"),
+  checkout: (Time.now + (60 * 60 * 24 * 7 * 2)).strftime("%F"),
+  hotel_ids: [303762]
+}
+response = Booking::Client.new.get_hotel_availabillity(request_parameters: params)
+
+# the parsed response is in the body
+=> response.body
+
+{
+  "checkout": "2016-04-04",
+  "hotels": [
+    {
+      "room_min_price": {
+        "price": "750.00"
+      },
+      "hotel_id": "303762",
+      "hotel_currency_code": "EUR"
+    }
+  ],
+  "checkin": "2016-03-21",
+  "guest_groups": [
+    {
+      "guests": 2,
+      "children": [
+
+      ]
+    }
+  ],
+  "hotel_ids": [
+    "303762"
+  ]
+}
+
+```
+
+## FAQ
+To get access to the API you have to signup as an affiliate for booking.com. They will then send you the API documentation with credentials to obtain data.
--- lib/booking/client.rb
+++ lib/booking/client.rb
@@ -9,9 +9,50 @@ module Booking
       @http_service
     end
 
-    def get_hotel_availabillity(opts: {})
-      http_service.request_post("/json/getHotelAvailabilityV2", opts)
+    def get_hotel_availabillity(request_parameters: {})
+      default_parameters = {
+        room1: "A,A",
+      }
+      http_service.request_post("/json/getHotelAvailabilityV2", default_parameters.merge(request_parameters))
     end
+
+    private
 
   end
 end
+
+
+__END__
+
+params = {
+  checkin: Time.now.strftime("%F"),
+  checkout: (Time.now + (60 * 60 * 24 * 7 * 2)).strftime("%F"),
+  hotel_ids: [303762]
+}
+response = Booking::Client.new.get_hotel_availabillity(request_parameters: params)
+
+puts response.body
+{
+  "checkout": "2016-04-04",
+  "hotels": [
+    {
+      "room_min_price": {
+        "price": "750.00"
+      },
+      "hotel_id": "303762",
+      "hotel_currency_code": "EUR"
+    }
+  ],
+  "checkin": "2016-03-21",
+  "guest_groups": [
+    {
+      "guests": 2,
+      "children": [
+
+      ]
+    }
+  ],
+  "hotel_ids": [
+    "303762"
+  ]
+}
